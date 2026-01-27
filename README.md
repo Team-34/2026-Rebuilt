10c8241da8692c1dc95c70458ceb6da6d0b0bc8c
Kamren Walsh <kamrenwalsh@dpi.lcsk12.org>
2026-01-27 16:33:01 -0600

    merge fixies

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # 2026-Rebuilt
 Rebuilt: FRC game for the 2025–2026 season
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Motors
+
+| Subsystem | Motor | CAN IDs|
+| -- | -- | -- |
+| Intake | 1x CTR Minion | 10 - 19 |
+| Shooter | 2x CTR Minion | 20 - 29 |
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -46,7 +46,7 @@ public class Shooter extends SubsystemBase {
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
   public boolean exampleCondition() {
-    // Query some boolean state, such as a digital senso
+    // Query some boolean state, such as a digital sensor.
     return false;
   }
 
