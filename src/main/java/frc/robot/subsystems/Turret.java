a141c8a37275dd2c9f26133efb33a1723e3b19d0
Sam <samhelton95@gmail.com>
2026-01-29 19:56:00 -0600

    still working on limelight, EOM 1-29

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -8,11 +8,11 @@ import java.util.function.BooleanSupplier;
 
 import com.ctre.phoenix6.hardware.TalonFXS;
 
-
+import frc.robot.LimelightHelpers;
 
 public class Turret extends SubsystemBase {
     
-    TalonFXS turretMotor = new TalonFXS(1);
+    TalonFXS turretMotor = new TalonFXS(50);
 
     PIDController turretPID = new PIDController(0.5, 0.5, 0.5);
 
@@ -20,6 +20,15 @@ public class Turret extends SubsystemBase {
 
     public Turret() {
     }
+
+    // IDs for the apriltags on both outposts, using 1 for testing purposes
+    //int[] validIDs = {9, 10, 25, 26};
+    int[] validIDs = {1};
+
+    // Get the area of the target, in a percentage of 0 to 100
+    double ta = LimelightHelpers.getTA("");
+
+    LimelightHelpers.PoseEstimate poseEstimate;
 
     /**
      * @param setpoint The setpoint to move the turret to.
@@ -50,18 +59,19 @@ public class Turret extends SubsystemBase {
         );
     }
 
-
-/**
- * @return The periodic method of the turret. As of now, it should be constantly moving the turret to the setpoint.
- *
- */
-public void periodic() {
+    /**
+     * @return The periodic method of the turret. As of now, it should be constantly moving the turret to the setpoint.
+     *
+     */
+    public void periodic() {
         turretMotor.set(
             turretPID.calculate(
                 turretMotor.getPosition().getValueAsDouble()
                 , turretPID.getSetpoint()
             )
         );
+
+        LimelightHelpers.SetRobotOrientation("", 0, 0, 0, 0, 0, 0);
     }
 
 }
