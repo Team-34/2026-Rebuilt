ed6fe829432db51903adbdeb81a0ab371328e8b0
tony <tonymorellli37@gmail.com>
2026-01-29 18:09:52 -0600

    Fixed Minion code and added code for toggling the pneumatics on the intake.

--- Morelli/Intake/src/main/java/frc/robot/RobotContainer.java
+++ Morelli/Intake/src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,14 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+//import edu.wpi.first.wpilibj.DoubleSolenoid;
+//import edu.wpi.first.wpilibj.PneumaticsModuleType;
+//import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.ExampleSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,7 +24,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
+  private final Intake intake = new Intake();
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -43,12 +46,13 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+   // new Trigger(m_exampleSubsystem::exampleCondition)
+    //    .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    m_driverController.a().whileTrue(intake.runIn()).onFalse(intake.stop());
+    m_driverController.b().whileTrue(intake.runOut()).onFalse(intake.stop());
+    m_driverController.x().whileTrue(intake.toggleIntakePneumatics());
+
   }
 
   /**
---
+++ Morelli/Intake/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFXS;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class Intake extends SubsystemBase {
+  
+    public TalonFXS m_minionMotor = new TalonFXS(10);
+    public DutyCycleOut m_motorControl = new DutyCycleOut(10);
+
+    public void robotInit() {
+       
+    }
+
+    public void teleopPeriodic() {
+        // periodic code can go here
+    }
+
+    public Command runIn() {
+        return runOnce(() -> {
+             m_minionMotor.setControl(m_motorControl.withOutput(0.5));
+        });
+   
+    }
+
+    public Command runOut() {
+        
+        return runOnce(() -> {
+             m_minionMotor.setControl(m_motorControl.withOutput(-0.5));
+        });
+       
+    }
+
+    public Command stop() {
+        return runOnce(() -> {
+            m_minionMotor.setControl(m_motorControl.withOutput(0));
+        });
+    }
+
+      private final Solenoid intakeSingle = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+  private final DoubleSolenoid intakeDouble = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+
+    public Command toggleIntakePneumatics() {
+        intakeSingle.toggle();
+        intakeDouble.toggle();
+        return null;
+    }
+}
