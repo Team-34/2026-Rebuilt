5288777d53c10f5eb8ae288ffa5cf1a14caa5d76
Kale Olmsted <kolmsted2009@gmail.com>
2026-01-31 15:33:45 -0600

    Fixed Solomans comments.

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -32,14 +32,14 @@ public class Climber extends SubsystemBase {
   public Command extendCommand() {
     return runOnce(
         () -> {
-          leftMotor.setControl(leftMotorControl.withOutput(.25));
+          leftMotor.setControl(leftMotorControl.withOutput(0.25));
         });
   }
 
     public Command retractCommand() {
       return runOnce(
           () -> {
-            leftMotor.setControl(leftMotorControl.withOutput(-.25));
+            leftMotor.setControl(leftMotorControl.withOutput(-0.25));
           });
     }
 
@@ -55,23 +55,4 @@ public class Climber extends SubsystemBase {
     public void setSolenoidOff() {
       doubleSolenoid.set(DoubleSolenoid.Value.kOff);
     }
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
