0290fbd0cc9658c049698f5f47851fab8c396119
tony <tonymorellli37@gmail.com>
2026-02-05 18:13:35 -0600

    fixed casing of the identifier "motor"

--- src/main/java/frc/robot/subsystems/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Spindexer.java
@@ -6,25 +6,25 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Spindexer extends SubsystemBase {
-    public final TalonFX Motor = new TalonFX(55);
+    public final TalonFX motor = new TalonFX(55);
 
     private final DutyCycleOut control = new DutyCycleOut(0);
 
     public Command spin() {
         return runOnce(() -> {
-            Motor.setControl(this.control.withOutput(0.5));
+            motor.setControl(this.control.withOutput(0.5));
         });
     }
 
     public Command stop() {
         return runOnce(() -> {
-            Motor.setControl(this.control.withOutput(0));
+            motor.setControl(this.control.withOutput(0));
         });
     }
 
     public Command spinReverse() {
         return runOnce(() -> {
-            Motor.setControl(this.control.withOutput(-0.5));
+            motor.setControl(this.control.withOutput(-0.5));
         });
     }
 }
