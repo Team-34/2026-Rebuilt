1c291354433197c9ad0325133704930488521684
Kamren Walsh <kamrenwalsh@dpi.lcsk12.org>
2026-02-09 17:06:18 -0600

    UPDATED TO FULL KRAKENS

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,24 +4,19 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix6.hardware.TalonFXS;
-import com.ctre.phoenix6.configs.TalonFXSConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
-import com.ctre.phoenix6.signals.MotorArrangementValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.wpilibj.smartdashboard.*;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   enum Speed {
-    STOP(0.0), HALF(0.5), FULL(0.90);
+    STOP(0.0), HALF(0.5), FULL(1.0);
 
     public final double value;
 
@@ -31,50 +26,26 @@ public class Shooter extends SubsystemBase {
   }
 
   private Speed speed = Speed.STOP;
-  private final TalonFXS leftMotor = new TalonFXS(22);
-  private final TalonFXS rightMotor = new TalonFXS(21);
+  private final TalonFX master = new TalonFX(20);
+  private final TalonFX padwan = new TalonFX(21);
 
-  private final TalonFX kraken = new TalonFX(25);
-
-  public Shooter() {
-    // TalonFXSConfiguration left_config = new TalonFXSConfiguration();
-    // TalonFXSConfiguration right_config = new TalonFXSConfiguration();
-
-    TalonFXConfiguration kraken_config = new TalonFXConfiguration();
-
-    // right_config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
-    // left_config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
-
-    // left_config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-
-    // var left_config_result = this.leftMotor.getConfigurator().apply(left_config);
-    // var right_config_result = this.rightMotor.getConfigurator().apply(right_config);
-
-    kraken_config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    
-    kraken.set(this.speed.STOP.value);
-
-    // SmartDashboard.putString("left shooter motor config: ", left_config_result.getName());
-    // SmartDashboard.putString("right shooter motor config: ", right_config_result.getName());
-    this.leftMotor.set(this.speed.STOP.value);
-    //this.rightMotor.setControl(new Follower(this.leftMotor.getDeviceID(), MotorAlignmentValue.Opposed));
-    // this.leftMotor.set(this.speed.STOP.value);
-    // this.rightMotor.set(this.speed.STOP.value);
-
+  /**
+ * 
+ */
+public Shooter() {
+    TalonFXConfiguration master_config = new TalonFXConfiguration();
+    master_config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    master.getConfigurator().apply(master_config);
+    padwan.setControl(new Follower(master.getDeviceID(),MotorAlignmentValue.Opposed)); 
   }
-
+  //right motor should go clockwise
   /**
    * Cyles thought 3 different speeds. Stop -> Half -> Full -> Stop -> ...
    *
    * @returns The command that switches the speed to the next in the cycle.
    */
 
-  private void runMotor(double motor_speed) {
-    this.kraken.set(motor_speed);
-  }
-
-  public Command cycleSpeedCommand() {
-    // Inline construction of command goes here.
+  public Command cycleSpeedCommand() {   // Inline construction of command goes here.
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(() -> {
       this.speed = switch (this.speed) {
@@ -85,6 +56,9 @@ public class Shooter extends SubsystemBase {
       };
       runMotor(this.speed.value);
     });
+  }
+  private void runMotor(double motor_speed) {
+    this.master.set(motor_speed);
   }
 
   /**
@@ -100,7 +74,7 @@ public class Shooter extends SubsystemBase {
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Shooter Speed: ", kraken.getVelocity().getValueAsDouble());
+    SmartDashboard.putNumber("Shooter Speed: ", master.getVelocity().getValueAsDouble());
   }
 
   @Override
