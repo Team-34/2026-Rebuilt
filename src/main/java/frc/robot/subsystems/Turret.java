eed2d60fefcba69d7235d0429137e82a8b4e2968
Brad Collins <bsc@housecollins.com>
2026-02-09 16:45:55 -0600

    Fixed formatting

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -10,53 +10,32 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorArrangementValue;
 
 public class Turret extends SubsystemBase {
-    
-    private final TalonFXS motor = new TalonFXS(50);
+  private final TalonFXS motor = new TalonFXS(50);
+  private final PositionVoltage positionControl = new PositionVoltage(0);
 
-    private final PositionVoltage positionControl = new PositionVoltage(0);
+  public Turret() {
+    final var config = new TalonFXSConfiguration();
+    config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
+    config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive; // <-- May be removed if needed
 
-    public Turret() {
-        final var config = new TalonFXSConfiguration();
+    motor.getConfigurator().apply(config);
+  }
 
-        config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
+  /**
+   * @param setpoint The setpoint to move the turret to.
+   * @return Moves the turret to the setpoint.
+   */
+  public Command turretByPositionCommand(final double position) {
+    return runOnce(() -> {
+      motor.setControl(positionControl.withPosition(position));
+    });
+  }
 
-        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive; // <-- May be removed if needed
-        
-        motor.getConfigurator().apply(config);
-    }
-
-    /**
-     * @param setpoint The setpoint to move the turret to.
-     * @return Moves the turret to the setpoint.
-     */
-    public Command turretByPositionCommand(double position) {
-        return runOnce(() -> {
-          motor.setControl(positionControl.withPosition(position));
-        });
-    }
-
-    /**
-     * @param power The power to give to the motor.
-     * @return Moves the motor.
-     */
-    public Command turretByPowerCommand(double power) {
-        return runEnd
-        (
-           () -> {
-            motor.set(power);
-           },
-           () -> {
-            motor.stopMotor();
-           }
-        );
-    }
-
-    /**
-     * @return The periodic method of the turret. As of now, it should be constantly moving the turret to the setpoint.
-     *
-     */
-    public void periodic() {
-
-    }
-
+  /**
+   * @param power The power to give to the motor.
+   * @return Moves the motor.
+   */
+  public Command turretByPowerCommand(final double power) {
+    return runEnd(() -> motor.set(power), () -> motor.stopMotor());
+  }
 }
