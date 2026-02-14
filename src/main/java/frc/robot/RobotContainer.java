91adcb77bb402a0efbdafafe532ca1f9624a7c0f
solomon siefert <solomonsiefert@gmail.com>
2026-02-14 15:59:17 -0600

    update shooter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -89,8 +89,8 @@ public class RobotContainer {
     final var idle = new SwerveRequest.Idle();
     RobotModeTriggers.disabled().whileTrue(drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
-    joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-    joystick.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+    ////joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    ////joystick.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
@@ -117,8 +117,7 @@ public class RobotContainer {
     // joystick.x().onTrue(intake.toggle());
 
     joystick.rightTrigger().onTrue(shooter.cycleSpeedCommand());
-    joystick.a().whileTrue(shooter.setPrecent(0.5));
-    joystick.a().onFalse(shooter.setPrecent(0.0));
+    joystick.a().onTrue(shooter.setPercent(0.5));
     joystick.b().onTrue(shooter.setPosition());
 
 
--- src/main/java/frc/robot/subsystems/FXSShooter.java
+++ src/main/java/frc/robot/subsystems/FXSShooter.java
@@ -4,26 +4,26 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXSConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.TalonFXS;
+import com.ctre.phoenix6.signals.BrushedMotorWiringValue;
+import com.ctre.phoenix6.signals.ExternalFeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.MotorArrangementValue;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter extends SubsystemBase {
+public class FXSShooter extends SubsystemBase {
   enum Speed {
     STOP(0.0), HALF(0.5), FULL(1.0);
 
@@ -34,37 +34,50 @@ public class Shooter extends SubsystemBase {
     }
   }
 
+  // private final TalonSRX aimer = new TalonSRX(23); ////old object of TalonSRX
+  // motor for hood.
   private Speed speed = Speed.STOP;
-  private final TalonSRX hoodMotor = new TalonSRX(23); // hood motor
-  private final TalonFX master = new TalonFX(22); // left
-  private final TalonFX padawan = new TalonFX(21); // right
+  private final TalonFX master = new TalonFX(22);// left
+  private final TalonFX padawan = new TalonFX(21);// right
+  private final TalonFXS hoodMotor = new TalonFXS(24);// trajectory motor
 
-  private final Encoder hoodEncoder = new Encoder(1, 0); // built-in encoder on PlG
-  private final CANcoder externalCancoder = new CANcoder(25); // external CTRE encoder
+  private final Encoder hoodEncoder = new Encoder(1, 0);// built-in encoder on PlG
+  private final CANcoder externalCancoder = new CANcoder(25);// external CTRE encoder
+
+  private final DutyCycleOut hoodMotorControl = new DutyCycleOut(0);
 
   private final DigitalInput limitSwitch = new DigitalInput(4);
-
-  private final PIDController hoodPID = new PIDController(2.5, 0.0, 0.0); // PID for hood
-
-  private double setPoint = 0.0;
 
   /**
   * 
   */
-  public Shooter() {
+  public FXSShooter() {
     TalonFXConfiguration masterConfig = new TalonFXConfiguration();
+    TalonFXSConfiguration hoodConfiguration = new TalonFXSConfiguration();
     masterConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     master.getConfigurator().apply(masterConfig);
     padawan.setControl(new Follower(master.getDeviceID(), MotorAlignmentValue.Opposed));
 
-    hoodMotor.setInverted(true);
+    hoodConfiguration.ExternalFeedback.ExternalFeedbackSensorSource = ExternalFeedbackSensorSourceValue.RemoteCANcoder;
+    hoodConfiguration.ExternalFeedback.FeedbackRemoteSensorID = externalCancoder.getDeviceID();
+    hoodConfiguration.Slot0.withKP(10).withKI(0).withKD(0);
+    hoodConfiguration.Commutation.MotorArrangement = MotorArrangementValue.Brushed_DC;
+    hoodConfiguration.Commutation.BrushedMotorWiring = BrushedMotorWiringValue.Leads_A_and_B;
 
-    hoodMotor.configRemoteFeedbackFilter(externalCancoder.getDeviceID(), RemoteSensorSource.CANCoder, 0, 10);
-    hoodMotor.configSelectedFeedbackSensor(FeedbackDevice.RemoteSensor0, 0, 10);
-    // hoodMotor.configMotionCruiseVelocity(1000, 10);
-    // hoodMotor.configMotionAcceleration(500, 10);
-    setPoint = externalCancoder.getPosition().getValueAsDouble();
-    hoodPID.setSetpoint(setPoint);
+    hoodMotor.getConfigurator().apply(hoodConfiguration);
+
+    hoodMotor.setControl(hoodMotorControl.withOutput(Speed.STOP.value));
+
+    // aimer.config_kP(0, 10, 10);
+    // aimer.config_kI(0, 0.0, 10);
+    // aimer.config_kD(0, 0.0, 10);
+    // aimer.config_kF(0, 0.0, 10);
+    // aimer.configRemoteFeedbackFilter(external_cancoder.getDeviceID(),
+    // RemoteSensorSource.CANCoder, 0, 10);
+    // aimer.configSelectedFeedbackSensor(FeedbackDevice.RemoteSensor0, 0, 10);
+    // aimer.configMotionCruiseVelocity(1000, 10);
+    // aimer.configMotionAcceleration(500, 10);
+
   }
 
   // right motor should go clockwise
@@ -93,11 +106,11 @@ public class Shooter extends SubsystemBase {
 
   public Command setPosition() {
     return runOnce(() -> {
-      moveAimingMotorRot(1.0);
+      moveAimingMotorRot(2);
     });
   }
 
-  public Command setPercent(double run_speed) {
+  public Command setPrecent(double run_speed) {
     return runOnce(() -> {
       moveAimingMotorPercent(run_speed);
     });
@@ -105,19 +118,15 @@ public class Shooter extends SubsystemBase {
 
   private void moveAimingMotorRot(double motor_rot) {
 
-    this.setPoint = motor_rot;
-    // this.hoodMotor.setPosition(position);
+    double position = motor_rot;
+    this.hoodMotor.setPosition(position);
+    // this.aimer.set(TalonSRXControlMode.Position, position);
   }
 
   private void moveAimingMotorPercent(double motor_speed) {
 
-    this.hoodMotor.set(TalonSRXControlMode.PercentOutput, motor_speed);
-    // this.hoodMotor.setControl(hoodMotorControl.withOutput(motor_speed));
-  }
-
-  private void ZeroEncoder() {
-    externalCancoder.setPosition(0.0);
-    this.setPoint = 0.0;
+    this.hoodMotor.setControl(hoodMotorControl.withOutput(motor_speed));
+    // this.aimer.set(TalonSRXControlMode.PercentOutput, motor_speed);
   }
 
   /**
@@ -136,18 +145,10 @@ public class Shooter extends SubsystemBase {
     SmartDashboard.putNumber("Shooter Speed: ", master.getVelocity().getValueAsDouble());
     SmartDashboard.putNumber("built-in encoder units", hoodEncoder.get());
     SmartDashboard.putNumber("external encoder units", externalCancoder.getPosition().getValueAsDouble());
+    SmartDashboard.putNumber("Hood Motor pos: ", hoodMotor.get());
     SmartDashboard.putBoolean("limit switch: ", limitSwitch.get());
-    SmartDashboard.putNumber("Hood Motor pos: ", hoodMotor.getSelectedSensorPosition());
-    // hoodMotor.getSelectedSensorPosition());
-    var pose = MathUtil.clamp(hoodPID.calculate(externalCancoder.getPosition().getValueAsDouble(), setPoint), -1.0,
-        1.0);
-    this.hoodMotor.set(TalonSRXControlMode.PercentOutput, pose);
-    SmartDashboard.putNumber("Hood Motor Velocity: ", hoodPID.getSetpoint());
-    SmartDashboard.putNumber("Hood Motor Output: ", pose);
-
-    if (limitSwitch.get() == false) {
-      ZeroEncoder();
-    }
+    // SmartDashboard.putNumber("hood motor pos: ",
+    // aimer.getSelectedSensorPosition());
   }
 
   @Override
