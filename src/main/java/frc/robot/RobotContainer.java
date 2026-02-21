00f79cfda269a43c223d04cd6eeab506af42fcaf
Kale Olmsted <kolmsted2009@gmail.com>
2026-02-21 13:12:27 -0600

    LED code in progress

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -179,4 +179,7 @@ public class RobotContainer {
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   
+  public void disable() {
+    leds.turnOffLEDs();
+  }
 }
--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -5,11 +5,11 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class LEDs extends SubsystemBase {
   private final AddressableLED ledStrip = new AddressableLED(0);
   private final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(10);
-  private final LEDPattern rainbow = LEDPattern.rainbow(255, 128);
   
   LEDPattern red = LEDPattern.solid(Color.kRed);
 
@@ -18,5 +18,12 @@ public class LEDs extends SubsystemBase {
     red.applyTo(ledBuffer);
     ledStrip.setData(ledBuffer);
     ledStrip.start();
+  }
+  public Command turnOffLEDs() {
+    return runOnce(() -> {
+    LEDPattern black = LEDPattern.solid(Color.kBlack);
+    black.applyTo(ledBuffer);
+    ledStrip.setData(ledBuffer);
+    });
   }
 }
