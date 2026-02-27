1c3a928dcbf707941e5b0c1c405177d166691d4c
Kale Olmsted <kolmsted2009@gmail.com>
2026-02-26 19:43:31 -0600

    Changed allianceColor and made a solid(Color color)

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -17,38 +17,35 @@ public class LEDs extends SubsystemBase {
  
   public LEDs() {
     ledStrip.setLength(ledBuffer.getLength());
-    LEDPattern initColor = LEDPattern.solid(Color.kGreen);
-    initColor.applyTo(ledBuffer);
-    ledStrip.setData(ledBuffer);
+    solid(Color.kGreen);
     ledStrip.start();
     allianceColor(DriverStation.getAlliance());
   }
 
-  public void turnOff() {
-    LEDPattern black = LEDPattern.solid(Color.kBlack);
-    black.applyTo(ledBuffer);
+  public void solid(Color color) {
+    LEDPattern pattern = LEDPattern.solid(color);
+    pattern.applyTo(ledBuffer);
     ledStrip.setData(ledBuffer);
+  }
+
+  public void turnOff() {
+    solid(Color.kBlack);
   }
 
   public void red() {
-    LEDPattern red = LEDPattern.solid(Color.kRed);
-    red.applyTo(ledBuffer);
-    ledStrip.setData(ledBuffer);
+    solid(Color.kRed);
   }
 
   public void blue(){
-    LEDPattern blue = LEDPattern.solid(Color.kBlue);
-    blue.applyTo(ledBuffer);
-    ledStrip.setData(ledBuffer);
+    solid(Color.kBlue);
   }
 
   public void allianceColor(Optional<DriverStation.Alliance> alliance) {
-    if(DriverStation.isEnabled()) {
-      if(alliance.isPresent() && alliance.get() == Alliance.Red) {
+    alliance.ifPresent(a -> {
+      if (a == Alliance.Red)
         red();
-      } else if(alliance.isPresent() && alliance.get() == Alliance.Blue) {
+      else 
         blue();
-      }
-    }
+    });
   }
 }
