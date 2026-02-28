a642705d5ae7bf238aa294d97b840b537184de16
Kale Olmsted <kolmsted2009@gmail.com>
2026-02-27 22:58:31 -0600

    Fixed the if block and added more javadocs.

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -14,7 +14,12 @@ public class LEDs extends SubsystemBase {
   private final AddressableLED ledStrip = new AddressableLED(0);
   private final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(300);
 
- 
+  /**
+   * Called in RobotContainer constructor. Sets the length of the LED strip to the
+   * length of the LED buffer, sets the LED strip to a solid green color, starts
+   * the LED strip, and sets the LED strip to a solid red or blue color based on
+   * the alliance color.
+   */
   public LEDs() {
     ledStrip.setLength(ledBuffer.getLength());
     solid(Color.kGreen);
@@ -22,30 +27,51 @@ public class LEDs extends SubsystemBase {
     allianceColor(DriverStation.getAlliance());
   }
 
+  /**
+   * Called in all solid color methods to set the LED strip to a solid color. This
+   * helps reduce ammount of code duplication.
+   * 
+   * @param color
+   */
   public void solid(Color color) {
     LEDPattern pattern = LEDPattern.solid(color);
     pattern.applyTo(ledBuffer);
     ledStrip.setData(ledBuffer);
   }
 
+  /**
+   * Called in RobotContainer.disable(). Turns off the LED strip by setting it to
+   * a solid black color.
+   */
   public void turnOff() {
     solid(Color.kBlack);
   }
 
+  /**
+   * Called in LEDs.allianceColor() Sets the LED strip to a solid red color.
+   */
   public void red() {
     solid(Color.kRed);
   }
 
-  public void blue(){
+  /**
+   * Called in LEDs.allianceColor() Sets the LED strip to a solid blue color.
+   */
+  public void blue() {
     solid(Color.kBlue);
   }
 
+  /**
+   * Called in LEDs constructor. Sets the LED strip to a solid red or blue color
+   * based on the alliance color.
+   */
   public void allianceColor(Optional<DriverStation.Alliance> alliance) {
     alliance.ifPresent(a -> {
-      if (a == Alliance.Red)
+      if (a == Alliance.Red) {
         red();
-      else 
+      } else {
         blue();
+      }
     });
   }
 }
