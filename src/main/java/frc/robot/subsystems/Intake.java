0745ee8acd2c31c55502703d5056241085e03ec6
tony <tonymorellli37@gmail.com>
2026-03-30 18:36:52 -0500

    Started on the new intake code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,37 +1,49 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFXS;
+
+import static edu.wpi.first.units.Units.Inches;
+
 import com.ctre.phoenix6.configs.TalonFXSConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorArrangementValue;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.DoubleSolenoid; 
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.units.measure.Distance;
 
 public class Intake extends SubsystemBase {
-  private final DoubleSolenoid piston = new DoubleSolenoid(PneumaticsModuleType.REVPH, 15, 14);
+
+    enum DeploymentState {
+      DEPLOYED(Inches.of(10.7)), RETRACTED(Inches.of(0)), BUMPER(Inches.of(4));
+      
+      public final Distance value;
+      
+      DeploymentState(final Distance value) {
+        this.value = value;
+      }
+    }
 
   public final TalonFXS motor = new TalonFXS(60);
   public final DutyCycleOut motorControl = new DutyCycleOut(0);
+  public final TalonFXS motor2 = new TalonFXS(61);
+  public final PositionVoltage positionControl = new PositionVoltage(0);
+  public final double GEAR_RATIO = 2.0 / 5.0;
+
+  private DeploymentState deploymentState = DeploymentState.RETRACTED;
+
+  private double MotorUnitToBigGearUnits(double encoderUnits) {
+    return encoderUnits * GEAR_RATIO;
+  }
 
   public Intake() {
     final TalonFXSConfiguration config = new TalonFXSConfiguration();
     config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
     config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     motor.getConfigurator().apply(config);
-
-    piston.set(DoubleSolenoid.Value.kForward);
-  }
-
-  public boolean isDeployed() {
-    return piston.get() == DoubleSolenoid.Value.kReverse;
-  }
-
-  public boolean isRetracted() {
-    return piston.get() == DoubleSolenoid.Value.kForward;
   }
 
   public Command runIn() {
@@ -48,19 +60,24 @@ public class Intake extends SubsystemBase {
     });
   }
 
-  public Command toggle() {
+  public Command moveToInches(double inches) {
     return runOnce(() -> {
-      motor.setControl(motorControl.withOutput(0));
-      piston.toggle();
+      //
+    });
+  }
+
+    public Command toggle() {
+    return runOnce(() -> {
+      this.deploymentState = switch (this.deploymentState) {
+        case RETRACTED -> DeploymentState.BUMPER;
+        case BUMPER -> DeploymentState.DEPLOYED;
+        case DEPLOYED -> DeploymentState.RETRACTED;
+        default -> DeploymentState.RETRACTED;
+      };
     });
   }
 
   public void activate(final double speed) {
-    motor.setControl(motorControl.withOutput(isDeployed() ? speed : 0));
-  }
-
-  @Override
-  public void periodic() {
-    SmartDashboard.putBoolean("Intake Deployed", isDeployed());
+    motor.setControl(motorControl.withOutput(speed));
   }
 }
