eb5622480621148c9b22162fdc6a7c1296adba97
Brad Collins <bsc@housecollins.com>
2026-04-10 08:02:31 -0500

    Finised up tests for Game.isInAllianceZone()

--- src/test/java/frc/robot/subsystems/GameTest.java
+++ src/test/java/frc/robot/subsystems/GameTest.java
@@ -2,13 +2,16 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Feet;
 import static edu.wpi.first.units.Units.Inches;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -18,18 +21,52 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 public class GameTest {
 
   static Stream<Arguments> isInAllianceZone_testCases() {
-    return Stream.of();
+    final var blueGame = new StubGame();
+    blueGame.alliance = Optional.of(Alliance.Blue);
+
+    final var redGame = new StubGame();
+    redGame.alliance = Optional.of(Alliance.Red);
+
+    final var blueHubX = Game.Hub.blue.position().getMeasureX();
+    final var redHubX  = Game.Hub.red.position().getMeasureX();
+
+    final var justInsideBlueAllianceArea = new Pose2d(
+      new Translation2d(blueHubX.minus(Inches.of(1)), Feet.of(10)),
+      Rotation2d.kZero
+    );
+    final var justOutsideBlueAllianceArea = new Pose2d(
+      new Translation2d(blueHubX.plus(Inches.of(1)), Feet.of(10)),
+      Rotation2d.kZero
+    );
+
+    final var justInsideRedAllianceArea = new Pose2d(
+      new Translation2d(redHubX.plus(Inches.of(1)), Feet.of(10)),
+      Rotation2d.kZero
+    );
+    final var justOutsideRedAllianceArea = new Pose2d(
+      new Translation2d(redHubX.minus(Inches.of(1)), Feet.of(10)),
+      Rotation2d.kZero
+    );
+    return Stream.of(
+      Arguments.of(blueGame, justInsideBlueAllianceArea,  true),
+      Arguments.of(blueGame, justOutsideBlueAllianceArea, false),
+      Arguments.of(blueGame, justOutsideRedAllianceArea,  false),
+      Arguments.of(blueGame, justInsideRedAllianceArea,   false),
+
+      Arguments.of(redGame, justInsideBlueAllianceArea,  false),
+      Arguments.of(redGame, justOutsideBlueAllianceArea, false),
+      Arguments.of(redGame, justOutsideRedAllianceArea,  false),
+      Arguments.of(redGame, justInsideRedAllianceArea,   true)
+    );
   }
 
-  @Test
-  public void isInAllianceZone_indicatesWhetherATranslation2dIsInOurAllianceZone() {
-    final var uut = new StubGame();
-    uut.alliance = Optional.of(Alliance.Blue);
-    final var pose = new Pose2d(
-      new Translation2d(
-        Game.Hub.blue.position().getMeasureX().minus(Inches.of(1)),
-        Feet.of(10)),
-      Rotation2d.kZero);
-    assertTrue(uut.isInAllianceZone(pose));
+  @ParameterizedTest
+  @MethodSource("isInAllianceZone_testCases")
+  public void isInAllianceZone_indicatesWhetherATranslation2dIsInOurAllianceZone(
+    final Game uut,
+    final Pose2d pose,
+    final boolean expected
+  ) {
+    assertEquals(expected, uut.isInAllianceZone(pose));
   }
 }
